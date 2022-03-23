5496b372ce372434c39f1985066007f31b0217ea
mostafa20001379 <mostafa.ahoee1379@gmail.com>
2022-03-24 03:31:54 +0430

    Create project

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *   version="1.0.0",
+ *   title="Nahad Lesson Service Open Api",
+ *   description="Implementation of Swagger with in Laravel",
+ * )
+ *
+ * @OA\Server(
+ *   url=DEV_HOST_URL
+ * )
+ *
+ * @OA\SecurityScheme(
+ *   securityScheme="bearerAuth",
+ *   in="header",
+ *   name="Authorization",
+ *   type="apiKey",
+ *   scheme="basic",
+ *   bearerFormat="JWT",
+ * )
+ *
+ */
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
---
+++ app/Http/Controllers/ElasticsearchController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Elasticsearch\ClientBuilder;
+
+class ElasticsearchController extends Controller
+{
+    public function elasticsearchQueries(Request $request)
+    {
+
+    }
+}
